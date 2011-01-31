a961aa4e384d8f63cb7ab9b61a013f4b83ea2de6
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2011-01-31 01:30:02 +0100

    more work on encryption

--- src/TestClient.java
+++ src/TestClient.java
@@ -1,8 +1,9 @@
 import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.net.Socket;
+import java.security.KeyStore;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,21 +11,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.net.SocketFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 import com.lekebilen.quasseldroid.qtcomm.QDataInputStream;
 import com.lekebilen.quasseldroid.qtcomm.QDataOutputStream;
 import com.lekebilen.quasseldroid.qtcomm.QMetaType;
 import com.lekebilen.quasseldroid.qtcomm.QMetaTypeRegistry;
 import com.lekebilen.quasseldroid.qtcomm.QVariant;
-import com.lekebilen.quasseldroid.qtcomm.serializers.QMap;
 
 
 public class TestClient {
 	public static void main(String[] args) {
 		try {
 			SocketFactory factory = (SocketFactory)SocketFactory.getDefault();
-			Socket socket = (Socket)factory.createSocket("mts.ms", 4242);
+			Socket socket = (Socket)factory.createSocket("localhost", 4242);
 			QDataOutputStream ss = new QDataOutputStream(socket.getOutputStream());
 			
 			Map<String, QVariant<?>> initial = new HashMap<String, QVariant<?>>();
@@ -71,13 +76,100 @@ public class TestClient {
 			 // We should check that the core is new and dandy here. 
 			 
 			// Now SMACK DAB ENCRYPTION YO
-			SSLSocketFactory sslSocketFactory = (SSLSocketFactory)SSLSocketFactory.getDefault();
-			socket = sslSocketFactory.createSocket(socket, "mts.ms", 4242, true);
+			SSLContext sslContext = SSLContext.getInstance("SSL");
+			sslContext.init(null, new CustomTrustManager[] {}, null);
+			
+			SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+			
+			SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(socket, "localhost", 4242, true);
+			sslSocket.setEnabledProtocols(new String[] {"SSLv3"});
+			for (String protocol: sslSocket.getEnabledProtocols()) {
+				System.out.println(protocol);
+			}
+			sslSocket.setUseClientMode(true);
+			sslSocket.startHandshake();
+			
 			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private class CustomTrustManager implements javax.net.ssl.X509TrustManager {
+	     /*
+	      * The default X509TrustManager returned by SunX509.  We'll delegate
+	      * decisions to it, and fall back to the logic in this class if the
+	      * default X509TrustManager doesn't trust it.
+	      */
+	     X509TrustManager sunJSSEX509TrustManager;
+	 
+	     CustomTrustManager() throws Exception {
+	         // create a "default" JSSE X509TrustManager.
+	 
+	         KeyStore ks = KeyStore.getInstance("JKS");
+	         ks.load(new FileInputStream("trustedCerts"),
+	             "passphrase".toCharArray());
+	 
+	         TrustManagerFactory tmf =
+	TrustManagerFactory.getInstance("SunX509", "SunJSSE");
+	         tmf.init(ks);
+	 
+	         TrustManager tms [] = tmf.getTrustManagers();
+	 
+	         /*
+	          * Iterate over the returned trustmanagers, look
+	          * for an instance of X509TrustManager.  If found,
+	          * use that as our "default" trust manager.
+	          */
+	         for (int i = 0; i < tms.length; i++) {
+	             if (tms[i] instanceof X509TrustManager) {
+	                 sunJSSEX509TrustManager = (X509TrustManager) tms[i];
+	                 return;
+	             }
+	         }
+	 
+	         /*
+	          * Find some other way to initialize, or else we have to fail the
+	          * constructor.
+	          */
+	         throw new Exception("Couldn't initialize");
+	     }
+	 
+	     /*
+	      * Delegate to the default trust manager.
+	      */
+	     public void checkClientTrusted(X509Certificate[] chain, String authType)
+	                 throws CertificateException {
+	         try {
+	             sunJSSEX509TrustManager.checkClientTrusted(chain, authType);
+	         } catch (CertificateException excep) {
+	             // do any special handling here, or rethrow exception.
+	         }
+	     }
+	 
+	     /*
+	      * Delegate to the default trust manager.
+	      */
+	     public void checkServerTrusted(X509Certificate[] chain, String authType)
+	                 throws CertificateException {
+	         try {
+	             sunJSSEX509TrustManager.checkServerTrusted(chain, authType);
+	         } catch (CertificateException excep) {
+	             /*
+	              * Possibly pop up a dialog box asking whether to trust the
+	              * cert chain.
+	              */
+	         }
+	     }
+	 
+	     /*
+	      * Merely pass this through.
+	      */
+	     public X509Certificate[] getAcceptedIssuers() {
+	         return sunJSSEX509TrustManager.getAcceptedIssuers();
+	     }
+		
 	}
 
 }
