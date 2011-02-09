6e142c3037fa872fe731dbd03dce6176e0742274
kenborge <kenborge@Andor>
2011-02-09 18:09:21 +0100

    Changed irc message to compare with messageId instead of timestamp

--- src/com/lekebilen/quasseldroid/IrcMessage.java
+++ src/com/lekebilen/quasseldroid/IrcMessage.java
@@ -65,7 +65,8 @@ public class IrcMessage implements Comparable<IrcMessage>{
 
 	
 	public int compareTo(IrcMessage other) {
-		return this.timestamp.compareTo(other.timestamp);
+		//return this.timestamp.compareTo(other.timestamp);
+		return ((Integer)messageId).compareTo((Integer)other.messageId);
 	}
 	
 	public String toString() {
