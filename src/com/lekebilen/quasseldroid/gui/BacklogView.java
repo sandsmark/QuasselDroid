763993aeabe66beceffd7d0876a067decbf41eb2
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2011-02-28 22:23:34 +0100

    start on a custom listview for backlog, so we can know when we want to fetch more backlog

---
+++ src/com/lekebilen/quasseldroid/gui/BacklogView.java
@@ -0,0 +1,29 @@
+package com.lekebilen.quasseldroid.gui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class BacklogView extends ListView {
+
+	public BacklogView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+	public BacklogView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BacklogView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	/*void onOverScrolled (int scrollX, int scrollY, boolean clampedX, boolean clampedY)
+	{
+		if (clampedY) fetch moar;
+	}*/
+	
+}
