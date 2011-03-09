1abd21f9706eef58fadfa4458bfc1f8aa60fcb2d
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2011-03-09 19:20:59 +0100

    fix sorting of buffers, in the worst possible way :D

--- src/com/lekebilen/quasseldroid/BufferCollection.java
+++ src/com/lekebilen/quasseldroid/BufferCollection.java
@@ -1,15 +1,17 @@
 package com.lekebilen.quasseldroid;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 public class BufferCollection extends Observable implements Observer {
 	
-	private SortedMap<Integer, Buffer> buffers = new TreeMap<Integer, Buffer>();
+	private HashMap<Integer, Buffer> buffers = new HashMap<Integer, Buffer>();
+	private List<Buffer> bufferList = new ArrayList<Buffer>();
 	
 	private static final String TAG = BufferCollection.class.getSimpleName();
 	
@@ -19,6 +21,8 @@ public class BufferCollection extends Observable implements Observer {
 	
 	public void addBuffer(Buffer buffer) {
 		buffers.put(buffer.getInfo().id, buffer);
+		bufferList.add(buffer);
+		Collections.sort(bufferList);
 		this.setChanged();
 		buffer.addObserver(this);
 		notifyObservers();
@@ -30,7 +34,7 @@ public class BufferCollection extends Observable implements Observer {
 	
 	public Buffer getPos(int pos) {
 		//TODO: might want to make something better then this. For when we want to change possitions and crap we need to be able to move stuff around
-		return (Buffer) buffers.values().toArray()[pos];
+		return (Buffer) bufferList.get(pos);
 	}
 	
 	public Buffer getBuffer(int bufferId) {
@@ -45,8 +49,10 @@ public class BufferCollection extends Observable implements Observer {
 	public void addBuffers(Collection<Buffer> buffers) {
 		for (Buffer buffer: buffers) {
 			this.buffers.put(buffer.getInfo().id, buffer);
+			bufferList.add(buffer);
 			buffer.addObserver(this);
 		}
+		Collections.sort(bufferList);
 		this.setChanged();
 		notifyObservers();
 	}
