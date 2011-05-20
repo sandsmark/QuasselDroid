0517292ef7879027096a244d2d94915adb70d1d6
Ken Viktil <kenborge@gmail.com>
2011-05-20 20:11:36 +0200

    Cleaned up previous commit, getting network info is now significant faster

--- src/com/lekebilen/quasseldroid/qtcomm/serializers/QByteArray.java
+++ src/com/lekebilen/quasseldroid/qtcomm/serializers/QByteArray.java
@@ -1,6 +1,10 @@
 package com.lekebilen.quasseldroid.qtcomm.serializers;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 
 import com.lekebilen.quasseldroid.qtcomm.DataStreamVersion;
 import com.lekebilen.quasseldroid.qtcomm.QDataInputStream;
@@ -8,8 +12,10 @@ import com.lekebilen.quasseldroid.qtcomm.QDataOutputStream;
 import com.lekebilen.quasseldroid.qtcomm.QMetaTypeSerializer;
 
 public class QByteArray implements QMetaTypeSerializer<String> {
-	static int buflen = -1;
-	static byte [] buf = null;
+	int buflen = -1;
+	ByteBuffer buf;
+	CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
+	CharBuffer charBuffer;
 	
 	@Override
 	public String unserialize(QDataInputStream stream, DataStreamVersion version)
@@ -18,13 +24,36 @@ public class QByteArray implements QMetaTypeSerializer<String> {
 		if(len == 0xFFFFFFFF)
 			return "";
 			
-		if (len > buflen) {
-			buf = new byte[len];
+		if (len > buflen) { // If the buffers we have are to small, make them bigger
+			buf = ByteBuffer.allocate(len);
+			charBuffer = CharBuffer.allocate(len);
 			buflen = len;
 		}
-		stream.readFully(buf, 0, len);
-
-		return new String(buf, 0, len, "UTF-8");
+		
+		//reset buffers, so we start from the beginning of them 
+		buf.clear();
+		charBuffer.clear();
+		
+		//mark the start so we can reset back after we have read in the string
+		charBuffer.mark();
+		
+		// Set the limit of the byte buffer, so we know where to stop the string. 
+		// Or else you get characters from old strings that was longer then this one
+		buf.limit(len);
+		
+		//Read the string
+		stream.readFully(buf.array(), 0, len);
+		
+		//Decode it with correct encoding
+		decoder.decode(buf, charBuffer, false);
+		
+		//Set where the current string ends, it is the position we are at after decoding into the buffer
+		charBuffer.limit(charBuffer.position());
+		
+		//Reset buffer back to the mark(the start of the buffer) so we can convert to string
+		charBuffer.reset();
+		
+		return charBuffer.toString();
 	}
 
 	@Override
