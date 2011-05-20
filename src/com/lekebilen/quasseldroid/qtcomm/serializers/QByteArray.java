a8f51bf32c7fba8ffaa1eeb17b6a6521bc78864b
Ken Viktil <kenborge@gmail.com>
2011-05-20 20:48:49 +0200

    moved string reading code into utility class so both QString and QBufferArray Can use it

--- src/com/lekebilen/quasseldroid/qtcomm/serializers/QByteArray.java
+++ src/com/lekebilen/quasseldroid/qtcomm/serializers/QByteArray.java
@@ -1,21 +1,16 @@
 package com.lekebilen.quasseldroid.qtcomm.serializers;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
 
 import com.lekebilen.quasseldroid.qtcomm.DataStreamVersion;
 import com.lekebilen.quasseldroid.qtcomm.QDataInputStream;
 import com.lekebilen.quasseldroid.qtcomm.QDataOutputStream;
 import com.lekebilen.quasseldroid.qtcomm.QMetaTypeSerializer;
+import com.lekebilen.quasseldroid.util.StringReaderUtil;
 
 public class QByteArray implements QMetaTypeSerializer<String> {
-	int buflen = -1;
-	ByteBuffer buf;
-	CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
-	CharBuffer charBuffer;
+	
+	StringReaderUtil stringReader = new StringReaderUtil("UTF-8");
 	
 	@Override
 	public String unserialize(QDataInputStream stream, DataStreamVersion version)
@@ -24,36 +19,7 @@ public class QByteArray implements QMetaTypeSerializer<String> {
 		if(len == 0xFFFFFFFF)
 			return "";
 			
-		if (len > buflen) { // If the buffers we have are to small, make them bigger
-			buf = ByteBuffer.allocate(len);
-			charBuffer = CharBuffer.allocate(len);
-			buflen = len;
-		}
-		
-		//reset buffers, so we start from the beginning of them 
-		buf.clear();
-		charBuffer.clear();
-		
-		//mark the start so we can reset back after we have read in the string
-		charBuffer.mark();
-		
-		// Set the limit of the byte buffer, so we know where to stop the string. 
-		// Or else you get characters from old strings that was longer then this one
-		buf.limit(len);
-		
-		//Read the string
-		stream.readFully(buf.array(), 0, len);
-		
-		//Decode it with correct encoding
-		decoder.decode(buf, charBuffer, false);
-		
-		//Set where the current string ends, it is the position we are at after decoding into the buffer
-		charBuffer.limit(charBuffer.position());
-		
-		//Reset buffer back to the mark(the start of the buffer) so we can convert to string
-		charBuffer.reset();
-		
-		return charBuffer.toString();
+		return stringReader.readString(stream, len);
 	}
 
 	@Override
--- src/com/lekebilen/quasseldroid/qtcomm/serializers/QString.java
+++ src/com/lekebilen/quasseldroid/qtcomm/serializers/QString.java
@@ -16,12 +16,11 @@ import com.lekebilen.quasseldroid.qtcomm.DataStreamVersion;
 import com.lekebilen.quasseldroid.qtcomm.QDataInputStream;
 import com.lekebilen.quasseldroid.qtcomm.QDataOutputStream;
 import com.lekebilen.quasseldroid.qtcomm.QMetaTypeSerializer;
+import com.lekebilen.quasseldroid.util.StringReaderUtil;
 
 public class QString implements QMetaTypeSerializer<String> {
-	int buflen = -1;
-	ByteBuffer buf;
-	CharsetDecoder decoder = Charset.forName("UTF-16BE").newDecoder();
-	CharBuffer charBuffer;
+	
+	StringReaderUtil stringReader = new StringReaderUtil("UTF-16BE");
 	
 	@Override
 	public void serialize(QDataOutputStream stream, String data,
@@ -40,36 +39,7 @@ public class QString implements QMetaTypeSerializer<String> {
 		int len = (int)stream.readUInt(32);
 		if(len == 0xFFFFFFFF)
 			return "";
-
-		if (len > buflen) { // If the buffers we have are to small, make them bigger
-			buf = ByteBuffer.allocate(len);
-			charBuffer = CharBuffer.allocate(len);
-			buflen = len;
-		}
 		
-		//reset buffers, so we start from the beginning of them 
-		buf.clear();
-		charBuffer.clear();
-		
-		//mark the start so we can reset back after we have read in the string
-		charBuffer.mark();
-		
-		// Set the limit of the byte buffer, so we know where to stop the string. 
-		// Or else you get characters from old strings that was longer then this one
-		buf.limit(len);
-		
-		//Read the string
-		stream.readFully(buf.array(), 0, len);
-		
-		//Decode it with correct encoding
-		decoder.decode(buf, charBuffer, false);
-		
-		//Set where the current string ends, it is the position we are at after decoding into the buffer
-		charBuffer.limit(charBuffer.position());
-		
-		//Reset buffer back to the mark(the start of the buffer) so we can convert to string
-		charBuffer.reset();
-		
-		return charBuffer.toString();
+		return stringReader.readString(stream, len);
 	}
 }
