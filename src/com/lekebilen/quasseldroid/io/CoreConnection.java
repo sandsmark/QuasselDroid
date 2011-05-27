6cfd7ee4950470ea4b944f653c079b5dd9f96b37
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2011-05-27 04:00:34 +0200

    add missing file

--- src/com/lekebilen/quasseldroid/io/CoreConnection.java
+++ src/com/lekebilen/quasseldroid/io/CoreConnection.java
@@ -340,6 +340,8 @@ public class CoreConnection {
 			inStream = new QDataInputStream(sslSocket.getInputStream());
 			outStream = new QDataOutputStream(sslSocket.getOutputStream());
 			socket = sslSocket;
+		} else {
+			System.out.println("CAKE NO SSL?????");
 		}
 		// FINISHED SSL CONNECTION
 
