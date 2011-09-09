e1b85e575c2d8ee52689df8463c050991160f45b
Ken Viktil <kenborge@gmail.com>
2011-09-10 01:05:39 +0200

    fixed bug in sorting of networks

--- src/com/iskrembilen/quasseldroid/BufferUtils.java
+++ src/com/iskrembilen/quasseldroid/BufferUtils.java
@@ -3,36 +3,41 @@ package com.iskrembilen.quasseldroid;
 import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.TextView.BufferType;
 
 public class BufferUtils {
 	public static int compareBuffers(Buffer buffer1, Buffer buffer2) {
-//		if (info.networkId != another.info.networkId)
-//			return info.networkId - another.info.networkId;
-		if (!(buffer1.isTemporarilyHidden() || buffer1.isPermanentlyHidden()) && (buffer2.isTemporarilyHidden() || buffer2.isPermanentlyHidden()))
-			return 1;
-		else if ((buffer1.isTemporarilyHidden() || buffer1.isPermanentlyHidden()) && !(buffer2.isTemporarilyHidden() || buffer2.isPermanentlyHidden()))
-			return -1;
-		else if (buffer1.isTemporarilyHidden() && !buffer2.isTemporarilyHidden())
-			return -1;
-		else if (!buffer1.isTemporarilyHidden() && buffer2.isTemporarilyHidden())
-			return 1;
-		else if (buffer1.isTemporarilyHidden() && !buffer2.isPermanentlyHidden())
-			return -1;
-		else if (!buffer1.isTemporarilyHidden() && buffer2.isPermanentlyHidden())
-			return 1;
-		else if ((buffer1.isTemporarilyHidden() && buffer2.isPermanentlyHidden())||(buffer1.isTemporarilyHidden() && buffer2.isTemporarilyHidden())) {
-			if (buffer1.getInfo().type != buffer2.getInfo().type)
-				return buffer1.getInfo().type.value - buffer2.getInfo().type.value;
-			else return buffer1.getInfo().name.compareToIgnoreCase(buffer2.getInfo().name);
-		} else if (!BufferCollection.orderAlphabetical)
-			return buffer1.getOrder() - buffer2.getOrder();
+		if(buffer1.getInfo().type == BufferInfo.Type.StatusBuffer && buffer2.getInfo().type == BufferInfo.Type.StatusBuffer) {
+			return buffer1.getInfo().networkId - buffer2.getInfo().networkId;
+		}
 		else {
-			if (buffer1.getInfo().type != buffer2.getInfo().type)
-				return buffer1.getInfo().type.value - buffer2.getInfo().type.value;
-			else return buffer1.getInfo().name.compareToIgnoreCase(buffer2.getInfo().name);
+
+			if (!(buffer1.isTemporarilyHidden() || buffer1.isPermanentlyHidden()) && (buffer2.isTemporarilyHidden() || buffer2.isPermanentlyHidden()))
+				return 1;
+			else if ((buffer1.isTemporarilyHidden() || buffer1.isPermanentlyHidden()) && !(buffer2.isTemporarilyHidden() || buffer2.isPermanentlyHidden()))
+				return -1;
+			else if (buffer1.isTemporarilyHidden() && !buffer2.isTemporarilyHidden())
+				return -1;
+			else if (!buffer1.isTemporarilyHidden() && buffer2.isTemporarilyHidden())
+				return 1;
+			else if (buffer1.isTemporarilyHidden() && !buffer2.isPermanentlyHidden())
+				return -1;
+			else if (!buffer1.isTemporarilyHidden() && buffer2.isPermanentlyHidden())
+				return 1;
+			else if ((buffer1.isTemporarilyHidden() && buffer2.isPermanentlyHidden())||(buffer1.isTemporarilyHidden() && buffer2.isTemporarilyHidden())) {
+				if (buffer1.getInfo().type != buffer2.getInfo().type)
+					return buffer1.getInfo().type.value - buffer2.getInfo().type.value;
+				else return buffer1.getInfo().name.compareToIgnoreCase(buffer2.getInfo().name);
+			} else if (!BufferCollection.orderAlphabetical)
+				return buffer1.getOrder() - buffer2.getOrder();
+			else {
+				if (buffer1.getInfo().type != buffer2.getInfo().type)
+					return buffer1.getInfo().type.value - buffer2.getInfo().type.value;
+				else return buffer1.getInfo().name.compareToIgnoreCase(buffer2.getInfo().name);
+			}
 		}
 	}
-	
+
 	public static void setBufferViewStatus(Context context, Buffer entry, TextView bufferView) {
 		//Check here if there are any unread messages in the buffer, and then set this color if there is
 		if (entry.hasUnseenHighlight()){
