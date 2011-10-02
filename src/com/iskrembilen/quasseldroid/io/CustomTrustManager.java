57f659b8de468e1f73c827ceb176780dae55d271
Sean McNamara <smcnam@gmail.com>
2011-10-02 14:12:07 +0800

    FindBugs code cleanups (5/13) - CustomTrustManager.
    CustomTrustManager: Here we reduce the size of the member data associated with each CustomTrustManager object by making the NewCertificateException static. If it's not static, then each CustomTrustManager has a child NewCertificateException object, which doesn't make a whole lot of sense (the exception doesn't read any instance data from CustomTrustManager).

--- src/com/iskrembilen/quasseldroid/io/CustomTrustManager.java
+++ src/com/iskrembilen/quasseldroid/io/CustomTrustManager.java
@@ -121,7 +121,7 @@ class CustomTrustManager implements javax.net.ssl.X509TrustManager {
 		return defaultTrustManager.getAcceptedIssuers();
 	}
 	
-	class NewCertificateException extends CertificateException {
+	static class NewCertificateException extends CertificateException {
 		private String hashedCert;
 		public NewCertificateException(String hashedCert) {
 			this.hashedCert = hashedCert;
