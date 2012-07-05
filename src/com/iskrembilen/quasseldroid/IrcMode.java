4c7a18908039f44bd2914d5530dea9990ca96d83
Stian Veum <themorn@gmail.com>
2012-07-05 20:40:23 +0200

    Add name to IrcMode

--- src/com/iskrembilen/quasseldroid/IrcMode.java
+++ src/com/iskrembilen/quasseldroid/IrcMode.java
@@ -1,23 +1,28 @@
 package com.iskrembilen.quasseldroid;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * User: Stian
  * Date: 05.07.12
  * Time: 20:14
  */
 public enum IrcMode {
-    OWNER ("q", 1),
-    ADMIN ("a", 2),
-    OPERATOR ("o", 3),
-    HALF_OPERATOR ("h", 4),
-    VOICE ("v", 5),
-    USER ("u", 6);
+    OWNER ("owner","q", 1),
+    ADMIN ("admin","a", 2),
+    OPERATOR ("operator","o", 3),
+    HALF_OPERATOR ("half operator","h", 4),
+    VOICE ("voice","v", 5),
+    USER ("user","u", 6);
 
-
+    public final String name;
     public final String shortName;
     public final int rank;
 
-    IrcMode(String shortName, int rank) {
+    IrcMode(String name, String shortName, int rank) {
+        this.name = name;
         this.shortName = shortName;
         this.rank = rank;
     }
