67819625e8a07bfcc747ddc432fead810a2afc5b
Stian Veum <themorn@gmail.com>
2012-07-06 12:12:36 +0200

    Add pluralization and iconresource to IrcMode

--- src/com/iskrembilen/quasseldroid/IrcMode.java
+++ src/com/iskrembilen/quasseldroid/IrcMode.java
@@ -14,21 +14,23 @@ public enum IrcMode {
      * Declare in order of rank, this way values() will naturally
      * return the different modes based on rank
      */
-    //TODO: Move static strings to strings.xml
-    //TODO: Add icon paths to the objects
-    OWNER ("owner","q"),
-    ADMIN ("admin","a"),
-    OPERATOR ("operator","o"),
-    HALF_OPERATOR ("half operator","h"),
-    VOICE ("voice","v"),
-    USER ("user",""); //This will work as a catch-all for unknown modes
+    OWNER ("Owner","q", "(s)", R.drawable.irc_operator),
+    ADMIN ("Admin","a", "(s)", R.drawable.irc_operator),
+    OPERATOR ("Operator","o", "(s)", R.drawable.irc_operator),
+    HALF_OPERATOR ("Half Operator","h", "(s)", R.drawable.irc_voice),
+    VOICE ("Voiced","v", "", R.drawable.irc_voice),
+    USER ("User","", "(s)", R.drawable.im_user); //This will work as a catch-all for unknown modes
 
     public final String modeName;
     public final String shortModeName;
+    public final String pluralization;
+    public final int iconResource;
 
-    IrcMode(String modeName, String shortModeName) {
+    IrcMode(String modeName, String shortModeName, String pluralization, int iconResource) {
         this.modeName = modeName;
         this.shortModeName = shortModeName;
+        this.pluralization = pluralization;
+        this.iconResource = iconResource;
     }
 
 }
