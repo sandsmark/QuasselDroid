8fb87bbb79d0e5ae42558a6d2c5e619369a612e9
Stian Veum <themorn@gmail.com>
2012-07-06 15:32:22 +0200

    Change name for Half Operator to Half-Op

--- src/com/iskrembilen/quasseldroid/IrcMode.java
+++ src/com/iskrembilen/quasseldroid/IrcMode.java
@@ -17,7 +17,7 @@ public enum IrcMode {
     OWNER ("Owner","q", "(s)", R.drawable.irc_operator),
     ADMIN ("Admin","a", "(s)", R.drawable.irc_operator),
     OPERATOR ("Operator","o", "(s)", R.drawable.irc_operator),
-    HALF_OPERATOR ("Half Operator","h", "(s)", R.drawable.irc_voice),
+    HALF_OPERATOR ("Half-Op","h", "(s)", R.drawable.irc_voice),
     VOICE ("Voiced","v", "", R.drawable.irc_voice),
     USER ("User","", "(s)", R.drawable.im_user); //This will work as a catch-all for unknown modes
 
