cdefd597ec47860495c015835e48302b2772cf24
kenborge <kenborge@gmail.com>
2012-08-04 20:17:06 +0200

    Removed service binding from Splash activity

--- src/com/iskrembilen/quasseldroid/gui/SplashActivity.java
+++ src/com/iskrembilen/quasseldroid/gui/SplashActivity.java
@@ -11,18 +11,22 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.crittercism.app.Crittercism;
 import com.iskrembilen.quasseldroid.R;
+import com.iskrembilen.quasseldroid.events.ConnectionChangedEvent;
+import com.iskrembilen.quasseldroid.events.ConnectionChangedEvent.Status;
 import com.iskrembilen.quasseldroid.service.CoreConnService;
+import com.iskrembilen.quasseldroid.util.BusProvider;
 import com.iskrembilen.quasseldroid.util.ThemeUtil;
+import com.squareup.otto.Subscribe;
 
 public class SplashActivity extends SherlockActivity {
 	// Set the display time, in milliseconds (or extract it out as a configurable parameter)
 	private final int SPLASH_DISPLAY_LENGTH = 500;
 	private final String TAG = SplashActivity.class.getSimpleName();
-	private boolean canBeFinished;
 	private Class<?> activityToStart;
 
 	@Override
@@ -41,80 +45,42 @@ public class SplashActivity extends SherlockActivity {
 
 	@Override
 	protected void onStart() {
-		canBeFinished = false;
-		super.onResume();
+		super.onStart();
+		BusProvider.getInstance().register(this);
 		new Handler().postDelayed(new Runnable(){
 
 			@Override
 			public void run() {
-				if(canBeFinished) {
+				if(activityToStart != null)
 					startActivity(activityToStart);
-				} else {
-					canBeFinished = true;
-				}
+				else startActivity(LoginActivity.class);
 			}
 		}, SPLASH_DISPLAY_LENGTH);
-		doBindService();
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
-		doUnbindService();
+		BusProvider.getInstance().unregister(this);
 	}
+
 	@Override
 	public void onBackPressed() {
 		//Eat back press not allowed here
 	}
 
 	private void startActivity(Class<?> activity) {
-		if(canBeFinished) {
-			Intent mainIntent = new Intent(SplashActivity.this, activity);
-			SplashActivity.this.startActivity(mainIntent);
-			finish();
+		Intent mainIntent = new Intent(SplashActivity.this, activity);
+		startActivity(mainIntent);
+		finish();
+	}
+
+	@Subscribe
+	public void onConnectionChanged(ConnectionChangedEvent event) {
+		if(event.status == Status.Connected || event.status == Status.Connecting) {
+			activityToStart = MainActivity.class;
 		} else {
-			activityToStart = activity;
-		}
-	}
-
-	private CoreConnService boundConnService = null;
-	private boolean isBound = false;
-	private ServiceConnection mConnection = new ServiceConnection() {
-
-		public void onServiceConnected(ComponentName className, IBinder service) {
-			// This is called when the connection with the service has been
-			// established, giving us the service object we can use to
-			// interact with the service. Because we have bound to a explicit
-			// service that we know is running in our own process, we can
-			// cast its IBinder to a concrete class and directly access it.
-			boundConnService = ((CoreConnService.LocalBinder)service).getService();
-			if(boundConnService.isConnected()) {
-				startActivity(MainActivity.class);
-			} else {
-				startActivity(LoginActivity.class);
-			}
-			canBeFinished = true;
-		}
-
-		public void onServiceDisconnected(ComponentName className) {
-			boundConnService = null;
-		}
-	};
-
-	void doBindService() {
-		// Establish a connection with the service. We use an explicit
-		// class name because we want a specific service implementation that
-		// we know will be running in our own process (and thus won't be
-		// supporting component replacement by other applications).
-		bindService(new Intent(SplashActivity.this, CoreConnService.class), mConnection, Context.BIND_AUTO_CREATE);
-		isBound = true;
-		Log.i(TAG, "Binding Service");
-	}
-
-	void doUnbindService() {
-		if (isBound) {
-			unbindService(mConnection);
-			isBound = false;
+			activityToStart = LoginActivity.class;
 		}
 	}
 }
