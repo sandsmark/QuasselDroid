ff94046713a383d95ad23e3325f7c1015a7dd10c
Ken Børge Viktil <kenborge@gmail.com>
2014-07-22 18:25:09 +0200

    changed join channel dialog to use dialog builder

--- QuasselDroid/src/main/java/com/iskrembilen/quasseldroid/gui/fragments/JoinChannelDialog.java
+++ QuasselDroid/src/main/java/com/iskrembilen/quasseldroid/gui/fragments/JoinChannelDialog.java
@@ -1,11 +1,15 @@
 package com.iskrembilen.quasseldroid.gui.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -38,31 +42,48 @@ public class JoinChannelDialog extends SherlockDialogFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.dialog_join_channel, container, false);
-
-        networkSpinner = (Spinner) root.findViewById(R.id.dialog_join_channel_network_spinner);
-        channelNameField = (EditText) root.findViewById(R.id.dialog_join_channel_channel_name_field);
-
-        OnClickListener buttonListener = new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (v.getId() == R.id.dialog_join_channel_cancel_button) {
-                    getDialog().dismiss();
-
-                } else if (v.getId() == R.id.dialog_join_channel_join_button && !channelNameField.getText().toString().equals("")) {
-                    String channelName = channelNameField.getText().toString().trim();
-                    String networkSelected = (String) networkSpinner.getSelectedItem();
-                    BusProvider.getInstance().post(new JoinChannelEvent(networkSelected, channelName));
-                    dismiss();
+    public void onStart() {
+        super.onStart();
+       AlertDialog dialog = (AlertDialog)getDialog();
+        if(dialog != null) {
+            Button positiveButton = (Button) dialog.getButton(Dialog.BUTTON_POSITIVE);
+            positiveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!channelNameField.getText().toString().equals("")) {
+                        String channelName = channelNameField.getText().toString().trim();
+                        String networkSelected = (String) networkSpinner.getSelectedItem();
+                        BusProvider.getInstance().post(new JoinChannelEvent(networkSelected, channelName));
+                        dismiss();
+                    }
                 }
-            }
-        };
-        root.findViewById(R.id.dialog_join_channel_join_button).setOnClickListener(buttonListener);
-        root.findViewById(R.id.dialog_join_channel_cancel_button).setOnClickListener(buttonListener);
+            });
+        }
+    }
 
-        return root;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View customLayout = inflater.inflate(R.layout.dialog_join_channel, null);
+
+        networkSpinner = (Spinner) customLayout.findViewById(R.id.dialog_join_channel_network_spinner);
+        channelNameField = (EditText) customLayout.findViewById(R.id.dialog_join_channel_channel_name_field);
+
+        builder.setView(customLayout);
+        builder.setPositiveButton(R.string.dialog_join_channel_ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //Needed for backwards compatibility
+            }
+        });
+        builder.setNegativeButton(R.string.dialog_join_channel_cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                getDialog().dismiss();
+            }
+        });
+
+        return builder.create();
     }
 
     public String[] getNetworkNames() {
