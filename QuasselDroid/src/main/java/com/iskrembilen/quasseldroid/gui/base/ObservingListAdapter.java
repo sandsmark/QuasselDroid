9e27fe7895c3f9f42858555ff74aac149a17f533
Janne Koschinski <janne@kuschku.de>
2014-12-28 23:05:21 +0100

    Added an error to the log if registering/unregistering of observers fails. Fixed https://github.com/sandsmark/QuasselDroid/pull/192#discussion_r22293787

--- QuasselDroid/src/main/java/com/iskrembilen/quasseldroid/gui/base/ObservingListAdapter.java
+++ QuasselDroid/src/main/java/com/iskrembilen/quasseldroid/gui/base/ObservingListAdapter.java
@@ -1,12 +1,16 @@
 package com.iskrembilen.quasseldroid.gui.base;
 
+import android.app.FragmentManager;
 import android.database.DataSetObservable;
 import android.database.DataSetObserver;
+import android.util.Log;
 import android.widget.ListAdapter;
 
 import java.util.Observer;
 
 public abstract class ObservingListAdapter implements ListAdapter, Observer {
+    private static final String TAG = ObservingListAdapter.class.getSimpleName();
+
     private final DataSetObservable mDataSetObservable = new DataSetObservable();
 
     @Override
@@ -14,7 +18,7 @@ public abstract class ObservingListAdapter implements ListAdapter, Observer {
         try {
             mDataSetObservable.registerObserver(observer);
         } catch (IllegalStateException e) {
-
+            Log.e(TAG, "Observer " + observer.getClass().getSimpleName() + " could not be registered to " + mDataSetObservable.getClass().getSimpleName());
         }
     }
 
@@ -23,7 +27,7 @@ public abstract class ObservingListAdapter implements ListAdapter, Observer {
         try {
             mDataSetObservable.unregisterObserver(observer);
         } catch (IllegalStateException e) {
-
+            Log.e(TAG, "Observer " + observer.getClass().getSimpleName() + " could not be unregistered from " + mDataSetObservable.getClass().getSimpleName());
         }
     }
 
