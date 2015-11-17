b065fd9c0721bd74e5d8fb815ba9a4d8dd71a336
Janne Koschinski <janne@kuschku.de>
2015-11-17 18:13:08 +0100

    Added first implementation for ability to modify LED color

--- QuasselDroid/src/main/java/com/iskrembilen/quasseldroid/util/QuasseldroidNotificationManager.java
+++ QuasselDroid/src/main/java/com/iskrembilen/quasseldroid/util/QuasseldroidNotificationManager.java
@@ -419,6 +419,8 @@ public class QuasseldroidNotificationManager {
 
                 if (defaults != 0) builder.setDefaults(defaults);
             }
+            
+            builder.setColor(Color.parseColor(preferences.getString(context.getString(R.string.preference_notification_light_color), context.getString(R.string.notification_light_color_default))));
 
             // Send the notification.
             notifyManager.notify(R.id.NOTIFICATION, builder.build());
