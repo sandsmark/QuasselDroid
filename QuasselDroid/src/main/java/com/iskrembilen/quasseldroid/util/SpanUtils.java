600c1d10aa37f12cfe9aa829fe37d326035ae00f
Steve Soltys <stevesoltys@live.com>
2017-08-27 20:53:23 -0400

    Fix #284

--- QuasselDroid/src/main/java/com/iskrembilen/quasseldroid/util/SpanUtils.java
+++ QuasselDroid/src/main/java/com/iskrembilen/quasseldroid/util/SpanUtils.java
@@ -28,6 +28,6 @@ import android.text.style.CharacterStyle;
 
 public class SpanUtils {
     public static void setFullSpan(Spannable text, CharacterStyle style) {
-        text.setSpan(style, 0, text.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        text.setSpan(style, 0, text.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
     }
 }
